d44f0f556f6ad25303142c8c94e88668862d68b7
imaginationbox09 <imaginationbox09@gmail.com>
2026-08-29 22:14:30 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Permite compilar a producción aunque existan advertencias/errores de TypeScript
     ignoreBuildErrors: true,
   },
 };
